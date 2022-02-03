e3b2a2b4babcae99b09e8abd91a859298f92c5b0
aviscainoc <aviscainoc@est.ups.edu.ec>
2022-02-03 14:11:28 -0500

    corrigiendo configuraciones

--- adaptacion_app/views.py
+++ adaptacion_app/views.py
@@ -11,5 +11,5 @@ def adaptador(request):
 
         texto_adaptado=texto_orignal 
 
-    return render(request, "adaptacion_app/contact.html", {'texto_adaptado':texto_adaptado})
+    return render(request, "/static/contact.html", {'texto_adaptado':texto_adaptado})
 
